d9f0ecea12f9664e46a36f499917139c9a14ac64
ihandouk <61102791+ihandouk@users.noreply.github.com>
2020-04-13 21:55:12 -0400

    Update CartTest.java

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -5,37 +5,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import main.java.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import main.java.Produce;
+import main.java.Cart;
+import static org.junit.Assert.assertEquals;
 
-import java.lang.reflect.Constructor;
-import java.util.Arrays;
-import java.util.Collection;
-
-@RunWith(Parameterized.class)
 public class CartTest {
 
-    private Class<Cart> classUnderTest;
 
-    @SuppressWarnings("unchecked")
-    public CartTest(Object classUnderTest) {
-        this.classUnderTest = (Class<Cart>) classUnderTest;
-    }
-
-    // Define all classes to be tested
-    @Parameterized.Parameters
-    public static Collection<Object[]> cartClassUnderTest() {
-        Object[][] classes = {
-            {Cart3.class}
-        };
-        return Arrays.asList(classes);
-    }
-
-    private Cart newCart(int age) throws Exception {
-        Constructor<Cart> constructor = classUnderTest.getConstructor(Integer.TYPE);
-        return constructor.newInstance(age);
-    }
-
+    
     // A sample Cart
 
     Cart cart1;
@@ -52,13 +29,16 @@ public class CartTest {
     double cart5Expected;
     double cart6Expected;
     double cart7Expected;
+    
+    main.java.Cart cart;
 
     @Before
     public void setUp() throws Exception {
+    	cart = new main.java.Cart(45);
         // all carts should be set up like this
 
         // cart created with an age 40 shopper
-        cart1 = newCart(45);
+        cart1 = new Cart(45);
         for (int i = 0; i < 2; i++) {
             cart1.addItem(new Alcohol());
         }
@@ -71,7 +51,7 @@ public class CartTest {
 
         cart1Expected = 0.0;
         
-        cart2 = newCart(18);
+        cart2 = new Cart(18);
         for(int i = 0; i<1; i++) {
         	cart2.addItem(new FrozenFood());
         }
@@ -81,7 +61,7 @@ public class CartTest {
         
         cart2Expected = 0.0;
         
-        cart3 = newCart(21);
+        cart3 = new Cart(21);
         for(int i = 0; i<3; i++) {
         	cart3.addItem(new Produce());
         }
@@ -94,7 +74,7 @@ public class CartTest {
         
         cart3Expected = 1.0;
         
-        cart4 = newCart(40);
+        cart4 = new Cart(40);
         for(int i = 0; i<2; i++) {
         	cart4.addItem(new FrozenFood());
         }
@@ -104,7 +84,7 @@ public class CartTest {
         
         cart4Expected = 1.0;
         
-        cart5 = newCart(46);
+        cart5 = new Cart(46);
         for(int i = 0; i<5; i++) {
         	cart5.addItem(new FrozenFood());
         	cart5.addItem(new Produce());
@@ -124,7 +104,7 @@ public class CartTest {
         
         cart5Expected = -4.0;
         
-        cart6 = newCart(25);
+        cart6 = new Cart(25);
         for(int i = 0; i<1; i++) {
         	cart6.addItem(new Dairy());
         }
@@ -137,7 +117,7 @@ public class CartTest {
         
         cart6Expected = 0.0;
         
-        cart7 = newCart(30);
+        cart7 = new Cart(30);
         for(int i = 0; i<1; i++) {
         	cart7.addItem(new Alcohol());
         }
@@ -152,6 +132,28 @@ public class CartTest {
     @After
     public void tearDown() throws Exception {
     }
+    
+    @Test
+    public void emptyCart() throws UnderAgeException{
+        assertEquals(0.0, cart.Amount_saved(), .01);
+    }
+    
+    @Test
+    public void produce_savings()throws UnderAgeException{
+        assertEquals(0.0, cart2.Amount_saved(), .01);
+        assertEquals(25.0, cart2.calcCost(), .01);
+
+    }
+    
+    @Test
+    public void taxCheck()throws UnderAgeException{
+        assertEquals(0.80, cart7.getTax(10, "AZ"), .01);
+        assertEquals(0.90, cart7.getTax(10, "CA"), .01);
+        assertEquals(0.7, cart7.getTax(10, "NY"), .01);
+        assertEquals(0.70, cart7.getTax(10, "CO"), .01);
+        assertEquals(10.0, cart7.getTax(10, "AZf"), .01);
+        }
+
     
 
     // sample test
