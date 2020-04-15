f7001567586045784fb1a620ee68fc3c7ae184c9
ihandouk <61102791+ihandouk@users.noreply.github.com>
2020-04-14 23:21:18 -0400

    Update CartTest.java

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import main.java.*;
+<<<<<<< HEAD
 import main.java.Produce;
 import main.java.Cart;
 import static org.junit.Assert.assertEquals;
@@ -13,6 +14,39 @@ public class CartTest {
 
 
     
+=======
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class CartTest {
+
+    private Class<Cart> classUnderTest;
+
+    @SuppressWarnings("unchecked")
+    public CartTest(Object classUnderTest) {
+        this.classUnderTest = (Class<Cart>) classUnderTest;
+    }
+
+    // Define all classes to be tested
+    @Parameterized.Parameters
+    public static Collection<Object[]> cartClassUnderTest() {
+        Object[][] classes = {
+            {Cart3.class}
+        };
+        return Arrays.asList(classes);
+    }
+
+    private Cart newCart(int age) throws Exception {
+        Constructor<Cart> constructor = classUnderTest.getConstructor(Integer.TYPE);
+        return constructor.newInstance(age);
+    }
+
+>>>>>>> Review
     // A sample Cart
 
     Cart cart1;
@@ -29,6 +63,7 @@ public class CartTest {
     double cart5Expected;
     double cart6Expected;
     double cart7Expected;
+<<<<<<< HEAD
     
     main.java.Cart cart;
 
@@ -39,6 +74,15 @@ public class CartTest {
 
         // cart created with an age 40 shopper
         cart1 = new Cart(45);
+=======
+
+    @Before
+    public void setUp() throws Exception {
+        // all carts should be set up like this
+
+        // cart created with an age 40 shopper
+        cart1 = newCart(45);
+>>>>>>> Review
         for (int i = 0; i < 2; i++) {
             cart1.addItem(new Alcohol());
         }
@@ -51,7 +95,11 @@ public class CartTest {
 
         cart1Expected = 0.0;
         
+<<<<<<< HEAD
         cart2 = new Cart(18);
+=======
+        cart2 = newCart(18);
+>>>>>>> Review
         for(int i = 0; i<1; i++) {
         	cart2.addItem(new FrozenFood());
         }
@@ -61,7 +109,11 @@ public class CartTest {
         
         cart2Expected = 0.0;
         
+<<<<<<< HEAD
         cart3 = new Cart(21);
+=======
+        cart3 = newCart(21);
+>>>>>>> Review
         for(int i = 0; i<3; i++) {
         	cart3.addItem(new Produce());
         }
@@ -74,7 +126,11 @@ public class CartTest {
         
         cart3Expected = 1.0;
         
+<<<<<<< HEAD
         cart4 = new Cart(40);
+=======
+        cart4 = newCart(40);
+>>>>>>> Review
         for(int i = 0; i<2; i++) {
         	cart4.addItem(new FrozenFood());
         }
@@ -84,7 +140,11 @@ public class CartTest {
         
         cart4Expected = 1.0;
         
+<<<<<<< HEAD
         cart5 = new Cart(46);
+=======
+        cart5 = newCart(46);
+>>>>>>> Review
         for(int i = 0; i<5; i++) {
         	cart5.addItem(new FrozenFood());
         	cart5.addItem(new Produce());
@@ -104,7 +164,11 @@ public class CartTest {
         
         cart5Expected = -4.0;
         
+<<<<<<< HEAD
         cart6 = new Cart(25);
+=======
+        cart6 = newCart(25);
+>>>>>>> Review
         for(int i = 0; i<1; i++) {
         	cart6.addItem(new Dairy());
         }
@@ -117,7 +181,11 @@ public class CartTest {
         
         cart6Expected = 0.0;
         
+<<<<<<< HEAD
         cart7 = new Cart(30);
+=======
+        cart7 = newCart(30);
+>>>>>>> Review
         for(int i = 0; i<1; i++) {
         	cart7.addItem(new Alcohol());
         }
@@ -133,6 +201,7 @@ public class CartTest {
     public void tearDown() throws Exception {
     }
     
+<<<<<<< HEAD
     @Test
     public void emptyCart() throws UnderAgeException{
         assertEquals(0.0, cart.Amount_saved(), .01);
@@ -155,6 +224,8 @@ public class CartTest {
         }
 
     
+=======
+>>>>>>> Review
 
     // sample test
     @Test
